8ff6ff418b3188c9e20a3d8425e0b09f8cebc1e8
MarVinCan <marcus.v.e.candido@gmail.com>
2022-03-31 19:12:18 -0300

    Demo - retrieving data (SQL)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
-		
+
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+
+			rs = st.executeQuery("select * from department");
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
 	}
 
 }
