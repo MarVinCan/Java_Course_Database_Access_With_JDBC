19ef34b09781997b554915f5779009a4c7686c62
MarVinCan <marcus.v.e.candido@gmail.com>
2022-04-02 17:20:31 -0300

    Demo - transactions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,37 +1,47 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		
 
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+"Id = ?");
+			st = conn.createStatement();
 			
-			st.setInt(1, 5);
+			conn.setAutoCommit(false);
 			
-			int rowsAffected = st.executeUpdate();
+			int row1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			System.out.println("Done! Rows Affected: "+ rowsAffected);
-		
+			//int x =1;
 			
+			//if(x < 2) {
+			//	throw new SQLException("Fake Error");
+			//}
+			
+			int row2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("Row 1: "+row1);
+			System.out.println("Row 2: "+ row2);
 		} 
 		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				conn.rollback();
+				System.out.println("Transaction rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				System.out.println("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
 		}
 		
 		finally {
